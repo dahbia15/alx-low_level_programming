599bcbbc453c983f1d0c9ca418df3d4ea9822f4c
dahbia15 <dahbiaelfajraoui@gmail.com>
2023-06-25 09:21:28 -0700

    commit

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - Determine if anumber is positive , negative or zero.
+ * 0 : is the number to be cheked
+ * Return: 0 on success
+ */
+void positive_or_negative(int i)
+	{
+		if (i < 0)
+	}
+print("%d is %s\n", i, "negative");
+{
+	else if (i > 0)
+}
+print("%d is %s\n", i, "positive");
+{
+	else
+}
+print("%d is %s\n", i, "zero");
+}
+return;
+}
