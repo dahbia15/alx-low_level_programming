6ca960d21b37c08b6be1e095af8bfd8084724968
dahbia15 <dahbiaelfajraoui@gmail.com>
2023-07-20 06:16:35 -0700

    up

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,41 +1,46 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "3-calc.h"
-/*
- * 0x0F. C - Function pointers
- * task 3
- */
-/**
- * main - prints its name, followed by a new line.
- * @argc: argument count
- * @argv: argument vector
- * Return: (0) success or (98, 99, 100) on field
- */
 
+/**
+ * main - program that perfroms simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
-	int (*pf)(int, int), n1, n2;
+	int arg1, arg2, result;
+	char o;
+	int (*func)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	pf = get_op_func(argv[2]);
-	if (pf == NULL)
+
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
+
+	func = get_op_func(argv[2]);
+
+	if (!func)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	n1 = atoi(argv[1]);
-	n2 = atoi(argv[3]);
-	if (((argv[2][0] == '/') || (argv[2][0] == '%')) && (n2 == 0))
+	o = *argv[2];
+
+	if ((o == '/' || o == '%') && arg2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	printf("%d\n", pf(n1, n2));
+
+	result = func(arg1, arg2);
+
+	printf("%d\n", result);
+
 	return (0);
 }
